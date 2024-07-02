345a9c658784e5bff05d8b7f7812f05fcd6642ea
GamingClaus <yujansharma20621129@gmail.com>
2024-07-02 20:40:09 +0545

    Added final  reults for the info

--- date-converter/src/main/java/DateFrame.java
+++ date-converter/src/main/java/DateFrame.java
@@ -1,10 +1,19 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,24 +27,40 @@ import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DateFrame extends JFrame {
+public class DateFrame extends JFrame implements ActionListener,KeyListener{
+    
+    Color[] colors = { Color.RED,
+        Color.BLUE,
+        Color.GREEN,
+        Color.ORANGE,
+        Color.YELLOW,
+        Color.CYAN,
+        Color.MAGENTA,
+        Color.PINK,
+        Color.LIGHT_GRAY,
+        Color.DARK_GRAY,
+        Color.BLACK,
+        Color.WHITE};
+    int colorindex = 0;
     
     JPanel topPanel;
     JPanel centerPanel;
+    JPanel radioPanel;
+    JPanel comboPanel;
     
     JLabel title;
-    
+    JLabel resultlabel;
+
     JRadioButton englishdate; // radiobutton which gets to sets the combobox for nepali to englsih date
     JRadioButton nepalidate; // radiobutton which gets to set the combobox to convert english to nepali date 
     
     JComboBox EnglishYear;
-    String[] engyears; 
     JComboBox EnglishMonth;
     JComboBox EnglishDay;
+    
 
-    JComboBox NepaliYear;
-    JComboBox NepaliMonth;
-    JComboBox NepaliDay;
+    JButton submit;
+
 
     DateFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,7 +78,19 @@ public class DateFrame extends JFrame {
         title = new JLabel();
         title.setText("DATE CONVERTER");
         title.setFont(new Font("Caslon",Font.BOLD,30));
-        title.setForeground(Color.BLACK);
+       
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                title.setForeground(colors[colorindex]);
+                colorindex = (colorindex + 1) % colors.length;
+
+            }
+            
+        }, 0,5000);
 
         topPanel.add(title);
         //--------------------------------------------------------------
@@ -62,16 +99,20 @@ public class DateFrame extends JFrame {
 
         centerPanel = new JPanel();
         centerPanel.setBorder(new EmptyBorder(25,10,10,10));
-        centerPanel.setLayout(new FlowLayout());
-        centerPanel.setBackground(null);
-        centerPanel.setOpaque(true);
-        centerPanel.setPreferredSize(new Dimension(400,400));
+        centerPanel.setLayout(new BoxLayout(centerPanel,BoxLayout.Y_AXIS));
+        //centerPanel.setBackground(null);
+        // centerPanel.setOpaque(true);
+        //centerPanel.setPreferredSize(new Dimension(400,400));
         
+        
+        radioPanel = new JPanel();
+        radioPanel.setLayout(new BoxLayout(radioPanel,BoxLayout.X_AXIS));
         //created a new button group so that the user can select one radio button at a time.
         ButtonGroup radiobuttonGroup = new ButtonGroup();
 
 
         englishdate = new JRadioButton();
+        englishdate.setSelected(true);
         englishdate.setText("Nepali To English Date");
         englishdate.setFont(new Font("Calibri",Font.PLAIN,15));
         englishdate.setFocusable(false);
@@ -85,14 +126,60 @@ public class DateFrame extends JFrame {
         radiobuttonGroup.add(englishdate);  
         radiobuttonGroup.add(nepalidate);
 
-        EnglishYear = new JComboBox<>();
+        radioPanel.add(englishdate);
+        radioPanel.add(nepalidate);
 
+        //creating a subpanel for the centerpanel
+        comboPanel = new JPanel();
+        //adding the english years to the combobox
+        int startyear = 1944;
+        int endyear = 2024;
+        int totalyears = endyear -startyear +1;
+    
+        String[] englishyear = new String[totalyears];
+    
+        for(int i = 0;i < totalyears;i++){
+            englishyear[i] = String.valueOf(startyear + i);
+        }
+        EnglishYear = new JComboBox<String>(englishyear);
+        EnglishYear.setFocusable(false);
+        
 
-        centerPanel.add(englishdate);
-        centerPanel.add(nepalidate);
+        int totalmonth = 12;
+        String[] englishmonth = new String[totalmonth];
+        for(int i = 0;i<totalmonth;i++){
+            englishmonth[i]= String.format("%02d",i+1);
+        }
+        EnglishMonth = new JComboBox<String>(englishmonth);
+        EnglishMonth.addActionListener(this);
+        EnglishMonth.setFocusable(false);
+       
+        int totaldays=31;
+        String[] englishday = new String[totaldays];
+        for(int i = 0;i<totaldays;i++){
+            englishday[i]= String.format("%02d",i+1);
+        }
+        EnglishDay = new JComboBox<>(englishday);
+        EnglishDay.setFocusable(false);
+
+        //used for submitting the total info 
+        submit = new JButton("Submit");
+        submit.setFocusable(false);
+        submit.addActionListener(this);
+        
+        resultlabel = new JLabel();
+
+        comboPanel.add(EnglishYear);
+        comboPanel.add(EnglishMonth);
+        comboPanel.add(EnglishDay);
+        comboPanel.add(submit);
+
+        centerPanel.add(radioPanel);
+        centerPanel.add(comboPanel);
+        centerPanel.add(resultlabel);
 
         //----------------------------------------------------------------
-
+        this.addKeyListener(this);
         this.add(centerPanel,BorderLayout.CENTER);
         this.add(topPanel,BorderLayout.NORTH);
         this.setVisible(true);
@@ -234,5 +321,55 @@ public class DateFrame extends JFrame {
     nepaliMap.put(2090, new int[] { 0, 30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30 });
     
     return nepaliMap.get(year)[month];
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==submit){
+            String selectedyear = EnglishYear.getSelectedItem()+"-"+EnglishMonth.getSelectedItem()+"-"+EnglishDay.getSelectedItem();
+
+            long diff = DifferenceEnglishAndNepaliDates("1944-01-01", selectedyear);
+            String NepaliDateConverted= addDifferencetoNepaliDate(diff);
+
+            centerPanel.removeAll();
+
+            JPanel resultPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            resultlabel.setText("The Converted Nepali Date is: " +NepaliDateConverted);
+            resultlabel.setFont(new Font("Calibiri",Font.BOLD,25));
+            
+            resultPanel.add(resultlabel);
+
+            centerPanel.add(resultPanel);
+            centerPanel.revalidate();
+            centerPanel.repaint();
+            
+           
+      }
+    }
+
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+      //no
+    }
+
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+      if(e.getKeyCode()== KeyEvent.VK_ENTER){
+        new DateFrame();
+        dispose();
+      }
+    }
+
+
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //no
     }
 }
--- date-converter/src/main/java/Main.java
+++ date-converter/src/main/java/Main.java
@@ -1,9 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        DateFrame dateFrame =  new DateFrame();
-        long diff= dateFrame.DifferenceEnglishAndNepaliDates("1944-01-01","2024-07-01");
-        System.out.println(dateFrame.addDifferencetoNepaliDate(diff));
+        new DateFrame();
+        
 
     }
 }
